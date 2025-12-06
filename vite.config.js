639d4e856551fdbb2e272e7f5fd65254b4bc41d2
Chih_Min_Su_PC\tomatomatotoma <6756536+toma1133@users.noreply.github.com>
2025-12-06 14:53:07 +0000

    Fixed project name

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd());
 
     return {
-        base: process.env.NODE_ENV === "production" ? "/trip-app/" : "/",
+        base: process.env.NODE_ENV === "production" ? "/travel-app/" : "/",
         envPrefix: ["VITE_", "SUPABASE_"],
         resolve: {
             alias: {
